71ed751bbd2429b348b7733cd08e25ec31b07994
44iggs44 <89277667+44iggs44@users.noreply.github.com>
2026-06-24 11:49:53 -0500

    created county level data set

--- fiber_crops/rudinrush/project_DVC.R
+++ fiber_crops/rudinrush/project_DVC.R
@@ -20,12 +20,14 @@
 # data file path
 data <- file.path(
     "","Users", "Izz2", "Library", "CloudStorage",
-    "OneDrive-SharedLibraries-WestVirginiaUniversity/Freedom Enyetornye - Fame_fiber")
+    "OneDrive-SharedLibraries-WestVirginiaUniversity/Freedom Enyetornye - Fame_fiber"
+)
 
 # list of packages to be downloaded or installed
 packages <- c(
      "fst", "lubridate", "dplyr", "sf", "readr", "stringr", "ggplot2", "fixest",
-     "did2s", "tmap", "data.table", "haven", "tigris", "RColorBrewer", "Cairo"
+     "did2s", "tmap", "data.table", "haven", "tigris", "RColorBrewer", "Cairo",
+     "maps"
 )
     
 # check for missing packages 
@@ -39,10 +41,62 @@ invisible(lapply(packages, library, character.only = TRUE))
 
 
 ########################################################################
-# - 1 lodd data get info
+# - 1 load data get info
 ########################################################################
 
-# create data object
+# create data object 
+fib_naics <- data.table::fread(
+    file = file.path(
+        data, "master_data", "fiber_manufacturing_all.csv")
+)
 
+# # create data object of one subset of data for data exp
+# test <- data.table::fread(
+#     file = file.path(
+#         data, "master_data", "2012.annual.by_industry",
+#         "2012.annual 3131 Fiber, yarn, and thread mills.csv"
+#     )
+# )
+
+# get state and county names from data object
+fbr_cnty_lvl <- fib_naics[, # no row operations
+    state_fips := str_sub(
+        area_fips, # create string of state fips codes
+        start = 1, 
+        end = 2
+        ) # select the first two characters
+][, # no row operations
+    county_fips := str_sub( #creates string of county fips codes
+        area_fips, # character variable
+        start = 3, # starts at 3rd character inclusive
+        end = 5 # ends at last character inclusive
+    )
+]
+
+# creates data table of fips codes for matching 
+tig_lookup <- unique(as.data.table(fips_codes)[, # no row operations 
+    .(state_code, state)
+    ]
+)
+
+# match state abbreviations to by joining 
+fbr_cnty_lvl <- fbr_cnty_lvl[
+    tig_lookup,
+    state_abb := i.state, # creates state abbreviation variable
+    on = .(state_fips = state_code) # joins when same state fips codes 
+]
+
+# create look up for counties
+tig_lookup <- unique(as.data.table(fips_codes)[,
+    .(state_code, county_code, county)
+    ]
+)
+
+# create variable of county names that
+fbr_cnty_lvl <- fbr_cnty_lvl[
+    tig_lookup,
+    county := i.county,
+    on = .(state_fips = state_code, county_fips = county_code)
+]
 
 
